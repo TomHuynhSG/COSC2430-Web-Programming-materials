f1b30931bc57f36773c92bc4559a920984415065
Tom Huynh <tomhuynhsg@gmail.com>
2023-04-09 22:07:56 +0700

    add comments

--- js-2/lecture/arrow-function/arrow-function.js
+++ js-2/lecture/arrow-function/arrow-function.js
@@ -1,17 +1,60 @@
+// Example 1
+// Without arrow function
 function square (x){
-    return x*x;
+    return x * x;
 }
 
+// With arrow function
 let square = (x) => {
-    return x*x;
+    return x * x;
 }
 
+// Example 2
+// Without arrow function
 function sum (x, y){
     return x + y;
 }
 
-
+// With arrow function
 let sum = (x, y) => {
     return x + y;
 }
+
+// Example 3
+// Round brackets are optional if there is only one parameter:
+let square = x => {
+    return x * x;
+}
+
+// Use empty round brackets for functions with no parameters:
+let sayHello = () => {
+    return "Hi there!";
+}
+
+// Example 4
+// Regular function expression
+let isEvenVersion1 = function (number) {
+    return number % 2 === 0;
+}
+
+// Arrow function with round brackets around parameters
+let isEvenVersion2 = (number) => {
+    return number % 2 === 0;
+}
+
+// No round brackets around parameters
+let isEvenVersion3 = number => {
+    return number % 2 === 0;
+}
+
+// Implicit return
+let isEvenVersion4 = number => {
+    number % 2 === 0;
+}
+
+// One-liner implicit return
+let isEvenVersion5 = number => number % 2 === 0;
+
+
+
 
---
+++ js-2/lecture/js-object/nested-array-object.js
@@ -0,0 +1,27 @@
+let shoppingCart = [
+    {
+        product: 'chocolate',
+        price: 6.5,
+        quantity: 1
+    },
+    {
+        product: 'chicken soup',
+        price: 12.45,
+        quantity: 2
+    },
+    {
+        product: 'toilet paper',
+        price: 2,
+        quantity: 20
+    }
+];
+
+let student = {
+    firstName: 'Tom',
+    lastName: 'Huynh',
+    hobbies: ['Music', 'Video Games'],
+    exams: {
+        midterm: 93,
+        final: 85
+    }
+};
