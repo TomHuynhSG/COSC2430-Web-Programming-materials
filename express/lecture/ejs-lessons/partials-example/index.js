57d67450a85d73408e294153f35ab3e56f2ac97e
Tom Huynh <tomhuynhsg@gmail.com>
2023-05-02 00:42:08 +0700

    add partials in ejs example

---
+++ express/lecture/ejs-lessons/partials-example/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
