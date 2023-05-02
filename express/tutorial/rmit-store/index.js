7fc7f4443afa7795421be30f99f7460144879eb8
Tom Huynh <tomhuynhsg@gmail.com>
2023-05-03 00:20:38 +0700

    polish tutorial exercise for express

--- express/tutorial/rmit-store/index.js
+++ express/tutorial/rmit-store/index.js
@@ -1,3 +1,5 @@
+// This load the products as a javascript object from products.js file
 const products = require('./products');
-
 console.log(products);
+
+// Fill in your code here to build NodeJS + Express + EJS web application
