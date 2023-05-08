35478496c8758fa8f768de015a680d3d43d40e96
Tom Huynh <tomhuynhsg@gmail.com>
2023-05-08 10:02:46 +0700

    update the examples

--- mongodb/lecture/mongoose/crud-example/index.js
+++ mongodb/lecture/mongoose/crud-example/index.js
@@ -39,42 +39,35 @@ const Product = mongoose.model('Product', productSchema);
 app.use(express.urlencoded({ extended: true }));
 
 // CREATE - Create a new product
-app.post('/products', async (req, res) => {
-  try {
-    const product = new Product(req.body);
-    console.log(req.body);
-    await product.save();
-    res.send(product);
-  } catch (error) {
-    res.send(error);
-  }
+app.post('/products', (req, res) => {
+  const product = new Product(req.body);
+  console.log(req.body);
+  product.save()
+    .then((product) => res.send(product))
+    .catch((error) => res.send(error));
 });
 
 // READ - Get all products
-app.get('/products', async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.send(products);
-  } catch (error) {
-    res.send(error);
-  }
+app.get('/products', (req, res) => {
+  Product.find({})
+    .then((products) => res.send(products))
+    .catch((error) => res.send(error));
 });
 
 // READ - Get a product by ID
-app.get('/products/:id', async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return res.send();
-    }
-    res.send(product);
-  } catch (error) {
-    res.send(error);
-  }
+app.get('/products/:id', (req, res) => {
+  Product.findById(req.params.id)
+    .then((product) => {
+      if (!product) {
+        return res.send();
+      }
+      res.send(product);
+    })
+    .catch((error) => res.send(error));
 });
 
 // UPDATE - Update a product by ID
-app.post('/products/:id/update', async (req, res) => {
+app.post('/products/:id/update', (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'price', 'description', 'onSale', 'categories'];
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
@@ -83,32 +76,32 @@ app.post('/products/:id/update', async (req, res) => {
     return res.send({ error: 'Invalid updates!' });
   }
 
-  try {
-    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!product) {
-      return res.send();
-    }
-    res.send(product);
-  } catch (error) {
-    res.send(error);
-  }
+  Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+    .then((product) => {
+      if (!product) {
+        return res.send();
+      }
+      res.send(product);
+    })
+    .catch((error) => res.send(error));
 });
 
+
 // DELETE - Delete a product by ID
-app.post('/products/:id/delete', async (req, res) => {
-  try {
-    const product = await Product.findByIdAndDelete(req.params.id);
-    if (!product) {
-      return res.send();
-    }
-    res.send(product);
-  } catch (error) {
-    res.send(error);
-  }
+app.post('/products/:id/delete', (req, res) => {
+  Product.findByIdAndDelete(req.params.id)
+    .then((product) => {
+      if (!product) {
+        return res.send();
+      }
+      res.send(product);
+    })
+    .catch((error) => res.send(error));
 });
+
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
--- mongodb/lecture/mongoose/crud-full-website-example/index.js
+++ mongodb/lecture/mongoose/crud-full-website-example/index.js
@@ -52,42 +52,36 @@ app.get('/product/new', (req, res) => {
 });
 
 // CREATE - Create a new product
-app.post('/product', async (req, res) => {
-  try {
-    console.log(req.body);
-    const product = new Product(req.body);
-    await product.save();
-    res.redirect('/products');
-  } catch (error) {
-    res.send(error);
-  }
+app.post('/product', (req, res) => {
+  console.log(req.body);
+  const product = new Product(req.body);
+  product.save()
+    .then(() => res.redirect('/products'))
+    .catch(error => res.send(error));
 });
 
 // READ - Get all products
-app.get('/products', async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.render('view-products', { products });
-  } catch (error) {
-    res.send(error);
-  }
+app.get('/products', (req, res) => {
+  Product.find({})
+    .then(products => res.render('view-products', { products }))
+    .catch(error => res.send(error));
 });
 
 // UPDATE - Show update product form
-app.get('/product/:id/update', async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return res.send();
-    }
-    res.render('update-product', { product });
-  } catch (error) {
-    res.send(error);
-  }
+app.get('/product/:id/update', (req, res) => {
+  Product.findById(req.params.id)
+    .then(product => {
+      if (!product) {
+        return res.send();
+      }
+      res.render('update-product', { product });
+    })
+    .catch(error => res.send(error));
 });
 
+
 // UPDATE - Update a product by ID
-app.post('/product/:id/update', async (req, res) => {
+app.post('/product/:id/update', (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'price', 'description', 'onSale', 'categories'];
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
@@ -96,45 +90,43 @@ app.post('/product/:id/update', async (req, res) => {
     return res.send({ error: 'Invalid updates!' });
   }
 
-  try {
-    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+  Product.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
-    });
-    if (!product) {
-      return res.send();
-    }
-    res.redirect('/products');
-  } catch (error) {
-    res.send(error);
-  }
+    })
+    .then(product => {
+      if (!product) {
+        return res.send();
+      }
+      res.redirect('/products');
+    })
+    .catch(error => res.send(error));
 });
 
 // DELETE - Show delete product form
-app.get('/product/:id/delete', async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return res.send();
-    }
-    res.render('delete-product', { product });
-  } catch (error) {
-    res.send(error);
-  }
+app.get('/product/:id/delete', (req, res) => {
+  Product.findById(req.params.id)
+    .then(product => {
+      if (!product) {
+        return res.send();
+      }
+      res.render('delete-product', { product });
+    })
+    .catch(error => res.send(error));
 });
 
 // DELETE - Delete a product by ID
-app.post('/product/:id/delete', async (req, res) => {
-  try {
-    const product = await Product.findByIdAndDelete(req.params.id);
-    if (!product) {
-      return res.send();
-    }
-    res.redirect('/products');
-  } catch (error) {
-    res.send(error);
-  }
+app.post('/product/:id/delete', (req, res) => {
+  Product.findByIdAndDelete(req.params.id)
+    .then(product => {
+      if (!product) {
+        return res.send();
+      }
+      res.redirect('/products');
+    })
+    .catch(error => res.send(error));
 });
+
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
