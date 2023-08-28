030a5830f54a2a9e6beec5fe4012e03217a2ba02
Tom Huynh <tomhuynhsg@gmail.com>
2023-08-28 09:54:41 +0700

    Update my-first-script.js

--- nodejs/lecture/my-first-node-project/my-first-script.js
+++ nodejs/lecture/my-first-node-project/my-first-script.js
@@ -1,6 +1,13 @@
 let a = 2;
 let b = 3;
+
+function doMath(a, b, c){
+    var d = a+b
+    return d*c
+}
+var result
 for (let c=0; c<a+b; c++){
-    console.log("Hello from the loop!");
+    result = doMath(a, b, c)
+    console.log("Hello from the loop! Result: " + result);
 }
 
