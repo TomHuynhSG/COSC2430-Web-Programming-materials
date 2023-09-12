c7da3d70874a346cc798eea28bbcb679a466dfb4
Tom Huynh <tom.huynh@rmit.edu.vn>
2023-09-12 17:47:48 +0700

    Update createData.js

--- mongodb/tutorial/solution/exercise-0/createData.js
+++ mongodb/tutorial/solution/exercise-0/createData.js
@@ -1,40 +1,38 @@
 const { User, Product } = require('./model');
 
 
-// const newUser = new User({
-//     firstName: 'John',
-//     lastName: 'Doe',
-//     email: 'john.doe@example.com',
-//     age: 30,
-// });
+const newUser1 = new User({
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    age: 30,
+});
 
-const newUser = new User({
+newUser1.save()
+.then(() => {
+    console.log('User saved successfully');
+})
+.catch(err => {
+    console.error('Validation error:', err.message);
+});
+
+const newUser2 = new User({
     email: 'john.doe@example.com',
     age: 30
 });
 
-newUser.save()
-    .then(() => {
-        console.log('User saved successfully');
-    })
-    .catch(err => {
-        console.error('Validation error:', err.message);
-    });
+newUser2.save()
+.then((user) => console.log(user))
+.catch((error) => console.log(error));
 
 
-
-// newUser.save()
-// .then((user) => console.log(user))
-// .catch((error) => console.log(error));
-
-
-// const newProduct = new Product({
-//     name: 'Sample Product',
-//     price: 100,
-//     category: 'Electronics',
-// });
+const newProduct = new Product({
+    name: 'Sample Product',
+    price: 100,
+    category: 'Electronics',
+});
 
 
-// newProduct.save()
-// .then((product) => console.log(product))
-// .catch((error) => console.log(error));
+newProduct.save()
+.then((product) => console.log(product))
+.catch((error) => console.log(error));
